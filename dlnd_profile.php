60a6f59e450f2b40aab115928b9620e67fb3dfed
cbartondock <cbartondock@gmail.com>
2022-06-05 17:49:49 -0400

    fixed download

--- dlnd_profile.php
+++ dlnd_profile.php
@@ -2,7 +2,7 @@
 session_start();
 if (isset($_SESSION['username']) && isset($_GET['filename'])) {
   $iuser = exec("whoami");
-  $file = '/home/'.$iuser.'/configs/'.$_GET['filename'];
+  $file = $_SERVER['PIVPNCONFS'].'/'.$_GET['filename'];
   header('Content-type: application/octet-stream');
   header('Content-Disposition: attachment; filename="'.$_GET['filename'].'"');
   header('Content-length: ' . filesize($file));
